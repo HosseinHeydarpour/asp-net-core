7ff249905cb78f09275d30c5897b98c550b15d74
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-07-07 17:03:14 +0330

    Query String

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -7,39 +7,22 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-
-
-
-    
-
-
-
     string path = context.Request.Path;
     string method = context.Request.Method;
 
     context.Response.Headers["Content-Type"] = "text/html";
 
+
+
     if (method == "GET") 
     {
-        await context.Response.WriteAsync($"<p style='color: red; font-size:32px;'>{method}</p> ");
+        if (context.Request.Query.ContainsKey("id")) {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p style='color: blue; font-size:32px;'>{id}</p> ");
+        }
     }
-
-
-    if(path == "/path1")
-    {
-        await context.Response.WriteAsync($"<h1 style='color: red; font-size:32px;'>PATH 1 ENABLED</h1> ");
-        //await context.Response.WriteAsync($"<p style='color: green; font-size:32px;'>{method}</p> ");
-
-    } else
-    {
-        await context.Response.WriteAsync($"<p style='color: red; font-size:32px;'>{path}</p> ");
-        //await context.Response.WriteAsync($"<p style='color: green; font-size:32px;'>{method}</p> ");
-    }
-
-    
-
-
-
+    await context.Response.WriteAsync($"<p style='color: red; font-size:32px;'>{path}</p> ");
+ 
 });
 
 
--- MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
+++ MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd366c97216467e29ce3d8deb85c70a29ff703e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0ba5fe6267d5ebad46da88133fe7452dc545b76")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
