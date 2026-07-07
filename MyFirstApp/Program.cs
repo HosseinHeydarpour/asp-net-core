6b4146bebd996760ac53e0ad3c6dc6e25b67cd7e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-07-07 14:22:37 +0330

    HTTP Status Codes

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -5,6 +5,31 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    //context.Response.StatusCode = 400;
+
+    if(1 == 1)
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("Success ");
+        
+    } else
+    {
+        context.Response.StatusCode = 400;
+        // response body
+        await context.Response.WriteAsync("Bad ");
+        await context.Response.WriteAsync("Request!!");
+
+    }
+
+
+
+
+});
+
+
+
+
 
 app.Run();
--- MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
+++ MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c226d956e53b5c3c82c24301ae477e38531dfa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2364779bff34e91fe00cbf95f59df3f394897e00")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
