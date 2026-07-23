f660d8913c2397b929232627ed77acb1cc6791eb
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-07-23 09:36:48 +0330

    intro to routing

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -4,10 +4,18 @@ using System.IO;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
-
-
 var app = builder.Build();
+
+
+// enable routing
+app.UseRouting();
+
+// creating end-points
+app.UseEndpoints(endpoints =>
+{
+    // add your end points - all methods which start with Map
+    
+});
 
 
 app.Run();
