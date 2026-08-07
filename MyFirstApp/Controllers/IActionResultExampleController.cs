c20c5fe4f8ff6870b517db35a83d083f37cbd712
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2026-08-07 12:43:20 +0330

    Status Code Results

--- MyFirstApp/Controllers/IActionResultExampleController.cs
+++ MyFirstApp/Controllers/IActionResultExampleController.cs
@@ -5,33 +5,46 @@ namespace MyFirstApp.Controllers
     public class IActionResultExampleController : Controller
     {
 
-        [Route("book")]
+        [Route("store/books")]
         // public ContentResult Index() - this definition of  the method gives us an error when returning the file content 
         public IActionResult Index()
         {
             if (!Request.Query.ContainsKey("bookid"))
             {
-                Response.StatusCode = 400;
+                //=================
+                //Response.StatusCode = 400;
+                //return Content("Book id is not supplied!");
+                //=================
 
-                return Content("Book id is not supplied!");
+                //return new BadRequestResult();
+                return BadRequest("Book id is not supplied!");
             }
 
             // Book id cannot be empty!
             if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
             {
-                Response.StatusCode = 400;
+                //Response.StatusCode = 400;
 
-                return Content("Book id cannot be null or empty!");
+                //return Content("Book id cannot be null or empty!");
+
+
+                return BadRequest("Book id cannot be null or empty!");
             }
 
             // Book id cannot be over 1000
             int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
-            if (bookId > 1000 || bookId<=0)
+            if (bookId > 1000 )
             {
-                Response.StatusCode = 400;
+                //Response.StatusCode = 400;
 
-                return Content("Book id cannot be over 1000 or below 0! It must be 1 and 1000");
+                //return Content("Book id cannot be over 1000 or below 0! It must be 1 and 1000");
 
+                return NotFound($"Book with id: {bookId} not found :( ");
+
+            } 
+            if(bookId <= 0)
+            {
+                return BadRequest("Book id cannot be negative or 0! It must be 1 and 1000");
             }
 
 
@@ -39,9 +52,14 @@ namespace MyFirstApp.Controllers
             bool isUserLoggedIn = Convert.ToBoolean(Request.Query["isloggedin"]);
             if (!isUserLoggedIn) 
             {
-                Response.StatusCode = 401;
+                //Response.StatusCode = 401;
 
-                return Content("User must be authenticatd...");
+                //return Content("User must be authenticatd...");
+
+
+                // return Unauthorized("User must be authenticatd...");
+
+                return StatusCode(401, "User must be authenticatd...");
             }
 
 
--- MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
+++ MyFirstApp/obj/Debug/net10.0/MyFirstApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d205cb1d4fff4f92d73d1d458839c949fc0dbcfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d47e9734e0c55a7a8527f48e2ae1a8824e2ac3b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
